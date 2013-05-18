8046ae50ba3434a2ca3c423bc6c05b7f55cfa5e5
Julian Cohen <julian@juco.co.uk>
2013-05-18 10:53:09 +0100

    View titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def full_title(title)
+		base_title = "Julian Cohen"
+		if !title.empty?
+			base_title = base_title + " - #{title}"
+		end
+		base_title
+	end
 end
--- spec/requests/me_spec.rb
+++ spec/requests/me_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 describe "Me" do
 
+	base_title = 'Julian Cohen'
+
 	describe "Home Page" do
 		before { visit root_path }
 		subject { page }
 
-		it { should have_selector('title', text: 'Julian Cohen - juco') }
+		it { should have_selector('title', text: base_title + ' - Home') }
 		it { should have_selector('h1', text: 'Julian Cohen') }
+		it { should have_selector('.photo-me') }
 
 	end
 
