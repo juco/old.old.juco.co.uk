6206a3fbd3685600c6bc4146c823a876357b79b8
Julian Cohen <julian@juco.co.uk>
2013-05-20 18:34:21 +0100

    Updated to named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Juco::Application.routes.draw do
-  get "me/home"
-  get "me/cv"
-  get "me/contact"
+  # get "me/home"
+  # get "me/cv"
+  # get "me/contact"
+
+  get "/home", to: "me#home"
+  get "/cv", to: "me#cv"
+  get "/blog", to: "me#blog"
+  get "/contact", to: "me#contact"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
