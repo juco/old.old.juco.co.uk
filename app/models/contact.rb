06036f431208216402aae48f1716e42fe5420303
Julian Cohen <julian@juco.co.uk>
2014-07-28 21:44:12 +0100

    Tidy

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :name, :email, :message 
+  attr_accessor :name, :email, :message
   validates :name, presence: true, length: { minimum: 3 }
   validates :email, length: { minimum: 5 }
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
@@ -21,7 +21,7 @@ class Contact
 
  def initialize(attributes={})
    attributes && attributes.each do |name, value|
-     send("#{name}=", value) if respond_to? name.to_sym 
+     send("#{name}=", value) if respond_to? name.to_sym
    end
  end
 
